2ab0f10eb0aef07a2faf7af757f209e0bd762f84
Joel Andarcia <38918563+JoelAndarcia@users.noreply.github.com>
2019-10-23 11:35:17 -0500

    Update cadenas.py

--- cadenas.py
+++ cadenas.py
@@ -23,3 +23,16 @@ print(palindrome(x))
 print(palindrome(y))
 print(palindrome(z))
 print(palindrome(v))
+
+##Otro método para cadenas
+f = open("asd.txt")
+
+aux = ""
+
+aux = f.readline()
+aux = f.readline()
+
+if aux == "asdf":
+    print("Iguales")
+
+print(aux)
